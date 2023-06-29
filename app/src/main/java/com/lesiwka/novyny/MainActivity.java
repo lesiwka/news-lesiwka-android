aafe3107f4611a65959ae487b49a270f2b60f68c
Mystic-Mirage <mm@m10e.net>
2023-06-29 22:06:51 +0300

    Disable haptic feedback

--- app/src/main/java/com/lesiwka/novyny/MainActivity.java
+++ app/src/main/java/com/lesiwka/novyny/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends Activity {
 
         mWebView.setOnLongClickListener(v -> true);
         mWebView.setLongClickable(false);
+        mWebView.setHapticFeedbackEnabled(false);
 
         mWebView.setWebViewClient(new MyWebViewClient());
 
